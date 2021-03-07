c7b593d2a6e1ef795769e0202998f35e93bb8b05
Nicholas-Mackiewicz <nicholas.h.mackiewicz@gmail.com>
2021-03-06 20:58:23 -0500

    Update Lab.py

--- Lab.py
+++ Lab.py
@@ -3,8 +3,9 @@ from tkinter import *
 
 ex1 = tkinter.Tk()
 ex1.title("EX1")
-label1 = tkinter.Label(ex1, text = "A",bg = "Red").pack(side = "left",fill=BOTH,expand=YES,)
-label3 = tkinter.Label(ex1, text = "C",bg = "blue").pack(side = "left",fill=BOTH,expand=YES,)
+label1 = tkinter.Label(ex1, text = "A",bg = "orange").pack(side = "left",fill=BOTH,expand=YES,)
+label2 = tkinter.Label(ex1, text = "B",bg = "red").pack(side = "left",fill=BOTH,expand=YES)
+label3 = tkinter.Label(ex1, text = "C",bg = "grey").pack(side = "left",fill=BOTH,expand=YES,)
 
 ex2 = tkinter.Tk()
 ex2.title("EX2")
